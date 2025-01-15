e77ce75e6175183275d9a1ef866341ae05b27458
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-15 18:52:10 -0300

    Número impa ou par sequencial

---
+++ Digitar.py
@@ -0,0 +1,8 @@
+x = 0
+while x < 5:
+    numero = int(input('Digite um número: '))
+    if numero % 2 == 0:
+        print('O número{numero} é par')
+    else:
+         print('O número{numero} é impar')
+         x = x + 1
