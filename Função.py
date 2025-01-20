d002bdbee0df1b44a6582ac05ef6b52cfe7ebbe3
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-20 19:47:26 -0300

    Calcular o raio de uma circuferência.

---
+++ Função.py
@@ -0,0 +1,25 @@
+#Dizer: Olá com uma função.
+# def dizerOla():
+#     print('Olá mundo')
+# def Dizerola(nome):
+#     print(f'Olá,{nome} mundo,rudo bom?')
+# nome = input('Digite seu nome: ')
+# diezerOla(nome)
+# def olaMundo():
+#     return 'Olá mundo,program em python'
+# def soma(x,y):
+#     result = x + y
+#     return result
+
+# def soma(a,b = 0):
+#     print(a + b)
+
+def CalcularCirculo(raio):
+    area = 3,14 * raio ** 2
+    print(f'A area é de:{area}')
+raio = int(input('Digite o raio: '))
+CalcularCirculo(raio)
+
+
+    
+            
--- Nome.py
+++ Nome.py
@@ -1,2 +1,8 @@
 nome = ['Ana','Maria','José','Calos']
 print(nome[1:3])
+nome = input('Digite seu nome: ')
+nome.append[nome]
+print(nome)
+nome.pop(0)
+print(nome)
+print(nome.index('Maria'))
