8711a354172caba8bf0cdc7d88a602a5a95908e8
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-21 20:23:20 -0300

    Produtos com separador de virgulas

--- Dicionario.py
+++ Dicionario.py
@@ -1,2 +1,14 @@
-listasdenome = ['Ana','Carlos','Caio','Davi']
-print(listasdenome[3])
+# listasdenome = ['Ana','Carlos','Caio','Davi']
+# print(listasdenome[3])
+# media = 0
+# n1 = n2 = n3 = n4 = 0.0
+# nome, idade = "Fabio",47
+
+# print(type(media))
+# x = 3
+# y = 4
+# print(not('y == x'))
+produto = 'Iphone'
+quantidade_estoque = 200
+
+print("O produto",produto,"tem",quantidade_estoque,"unidade no estoque",sep=",")
