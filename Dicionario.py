fac102c3211865985de30e2302b1f959d4189748
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-21 20:57:59 -0300

    Usando o range com contagem regressiva

--- Dicionario.py
+++ Dicionario.py
@@ -22,5 +22,13 @@
 # #     time.sleep(1)
 #     print("Acabou")
 
-lista = range(5)
-print(list(lista))
+lista = list(range(5))
+lista = list(range(1,6))
+lista = list(range(1,10,2))
+lista = list(range(5,0,-1))
+print(lista)
+
+import time 
+for i in range(5,0,-1):
+    print(i,end="\r")
+    time.sleep(i)
