2f773078fa759d3047eebeb12177049215bb2c54
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-21 20:33:58 -0300

    Contagem em segundos

--- Dicionario.py
+++ Dicionario.py
@@ -12,3 +12,11 @@ produto = 'Iphone'
 quantidade_estoque = 200
 
 print("O produto",produto,"tem",quantidade_estoque,"unidade no estoque",sep=",")
+
+import time
+print("Contagem")
+for i in range(5):
+    print(5 - i)
+    # print(5 - i, end="\r")
+    time.sleep(1)
+    print("Acabou")
