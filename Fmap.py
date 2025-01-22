7602e439c15054d913a2be379d6a2105c344c9c0
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-21 21:31:15 -0300

    Usando a soma 'sun

--- Fmap.py
+++ Fmap.py
@@ -1,13 +1,16 @@
-salarios = [1000,5000,7000,850]
+# salarios = [1000,5000,7000,850]
 
-# def aumentar_salario(salario):
-    # return salarios  * 1.1
-    # if salario > 3000:
-    #     novoSalario = salario * 1.8
-    # else:
-    #     novoSalario = salario * 1.1
-    #     return novoSalario
+# # def aumentar_salario(salario):
+#     # return salarios  * 1.1
+#     # if salario > 3000:
+#     #     novoSalario = salario * 1.8
+#     # else:
+#     #     novoSalario = salario * 1.1
+#     #     return novoSalario
     
-novoSalario = list(filter(lambda x : x > 2000,salarios))
-print(novoSalario)
+# novoSalario = list(filter(lambda x : x > 2000,salarios))
+# print(novoSalario)
   
+custos = [600,5000,350,4000]
+custosTotal = sum(custos)
+print(custosTotal)
