cba0f5dbaaa03c4a3ae91b08dba603b0d28a535f
Douglas Nunes <paulodnunes2016@gmail.com>
2025-01-26 11:41:01 -0300

    Adicionamdo,removendo e calcular a media da turma

---
+++ Aula 7/AddAluno.py
@@ -0,0 +1,61 @@
+def AdicionarAlunos(nome,notaportugues,notaMatematica,listaAlunos):
+    aluno = {'nome':nome,'portugues':notaportugues,'matematica':notaMatematica}
+    listaAlunos.append(aluno)
+def RemoverAlunos(nome,listaAlunos):
+    indice = 0
+    for aluno in listaAlunos:
+        if nome == aluno['nome']:
+            listaAlunos.pop(i)
+
+            i= i + 1
+
+def CalcularMedia(listaAlunos):
+    somaP = 0
+    somaM = 0
+
+    for aluno in listaAlunos:
+        somaP = somaP + aluno['potugues']
+        somaM = somaM + aluno['matematica']
+
+        media = somaP/len(listaAlunos)
+        media = somaM/len(listaAlunos)
+
+        print('A média de portugues foi de: {somaP}')
+        print('A média de matematica foi de: {somaM}')
+
+
+alunos = [
+    {'nome':'Luiz','portugues':10,'matematica':8},
+    {'nome':'Letica','portugues':10,'matematica':10},
+    {'nome':'André','portugues':6,'matematica':7}
+]
+
+while True:
+    opcao = input('Digite uma opcao\n1 - Adicionar Aluno e notas\n2 - Remover Aluno\n3 - Calcular a média da turma\n4 - Sair\n')
+   
+
+    if opcao == '4':
+        print('Sair')
+        break
+    elif opcao == '1':
+        nome = input('Digite o nome: ')
+        portugues = int(input('Digite a nota de portugues: '))
+        matematica = int(input('Digite a nota de mateatica: '))
+
+        AdicionarAlunos(nome,portugues,matematica,alunos)
+        print(alunos)
+
+    elif opcao == '2':
+        nome = input('Digite seu nome: ')
+        RemoverAlunos(nome,alunos)
+        print(alunos)
+
+    elif opcao == '3':
+        CalcularMedia(alunos)
+
+
+
+        
+                
+  
+    
