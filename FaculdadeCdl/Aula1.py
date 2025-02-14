28a1ded344aa219ea341894d0a875d8eed05d4c4
Douglas Nunes <paulodnunes2016@gmail.com>
2025-02-14 20:02:57 -0300

    Contagem do dado de 1 até 100

---
+++ FaculdadeCdl/Aula1.py
@@ -0,0 +1,38 @@
+import random
+
+c1 = 0
+c2 = 0
+c3 = 0
+c4 = 0
+c5 = 0
+c6 = 0
+
+for i in range(100):
+    numero = random.randint(1,6);
+    if numero == 1:
+        c1 = c1 + 1
+    elif numero == 2:
+        c2 = c2 + 1
+    elif numero == 3:
+            c3 = c3 + 1
+    elif numero == 4:
+        c4 = c4 + 1
+    elif numero == 5:
+        c5 = c5 + 1
+    elif numero == 6:
+        c6 = c6 + 1
+
+    print(c1 + c2 + c3 + c4 + c5 + c6)
+    print('Igual a:',c1,'%')
+    print('Igual a:',c1,'%')
+    print('Igual a:',c1,'%')
+    print('Igual a:',c1,'%')
+    print('Igual a:',c1,'%')
+    print('Igual a:',c1,'%')
+
+
+
+        
+        
+        
+        
