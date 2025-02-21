40e2bf2e5371d467a8e3b3112223f004082cd1a4
Douglas Nunes <paulodnunes2016@gmail.com>
2025-02-21 09:43:05 -0300

    Números pares e impares percentualCorigido

--- FaculdadeCdl/numerossorteados.py
+++ FaculdadeCdl/numerossorteados.py
@@ -1,27 +1,26 @@
 import random
-
 Numeros = []
 
-
-for i in range(100):
+for i in range(1,100):
     Numeros.append(random.randint(1,1000))
-    
-    Pares = 0
-    Impares = 0
+
+Pares = 0
+Impares = 0
+
+for Numeros in Numeros:
+    if Numeros %2 == 0:
+        Pares += 1
+    else:
+        Impares += 1
+
+Percentual_Pares = (Pares / 100) * 100
+Percentual_Impares = (Impares / 100) * 100
+
+print(f'Porcentagem de números Pares:{Percentual_Pares}%')
+print(f'Porcentagem de números Impares:{Percentual_Impares}%')
 
 
-    for numero in Numeros:
-        if numero %2 == 0:
-         Pares += 1
-        else:
-         Impares += 1
 
-
-    Percentual_Pares = (Pares / 100) *100
-    Percentual_Impares = (Impares / 100) *100
-
-    print(f'Porcentagem de números Pares:{Percentual_Pares}%')
-    print(f'Porcentagem de números Impares:{Percentual_Impares}%')
                         
 
     
