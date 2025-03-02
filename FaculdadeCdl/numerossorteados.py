d878d412a45638beb111dab6e4e5bc8918ebc2ce
Douglas Nunes <paulodnunes2016@gmail.com>
2025-03-02 15:01:46 -0300

    Fatorial

--- FaculdadeCdl/numerossorteados.py
+++ FaculdadeCdl/numerossorteados.py
@@ -1,26 +1,35 @@
-import random
-Numeros = []
+# import random
+# Numeros = []
 
-for i in range(1,100):
-    Numeros.append(random.randint(1,1000))
+# for i in range(1,100):
+#     Numeros.append(random.randint(1,1000))
 
-Pares = 0
-Impares = 0
+# Pares = 0
+# Impares = 0
 
-for Numeros in Numeros:
-    if Numeros %2 == 0:
-        Pares += 1
-    else:
-        Impares += 1
+# for Numeros in Numeros:
+#     if Numeros %2 == 0:
+#         Pares += 1
+#     else:
+#         Impares += 1
 
-Percentual_Pares = (Pares / 100) * 100
-Percentual_Impares = (Impares / 100) * 100
+# Percentual_Pares = (Pares / 100) * 100
+# Percentual_Impares = (Impares / 100) * 100
 
-print(f'Porcentagem de números Pares:{Percentual_Pares}%')
-print(f'Porcentagem de números Impares:{Percentual_Impares}%')
+# print(f'Porcentagem de números Pares:{Percentual_Pares}%')
+# print(f'Porcentagem de números Impares:{Percentual_Impares}%')
+
+numero = int(input('Digite um número: '))
+fatoria = 1
+for i in range(1,numero + 1):
+    fatoria *=i
+
+
+    print(f'O fatorial {numero} é {fatoria}')
 
 
 
-                        
+
+
 
     
