d4c07facf4ebc40dcfd3d6a13187ed25d41eaab4
Douglas Nunes <paulodnunes2016@gmail.com>
2025-03-14 20:47:44 -0300

    Usando vetoresr

--- FaculdadeCdl/Listas.py
+++ FaculdadeCdl/Listas.py
@@ -2,6 +2,13 @@
 # media = (notas[0] + notas[1] + notas[2] ) /3
 # notas = [8.0,5.5,1.4]
 
-notas = [8.0,5.5,1.4]
-for i in range(3):
-    print(notas[i])
+# notas = [8.0,5.5,1.4]
+# for i in range(3):
+#     print(notas[i])
+
+notas = []
+notas.append(float(input('\nDigite a primeira nota do aluno: ')))
+notas.append(float(input('\nDigite a segunda nota do aluno: ')))
+notas.append(float(input('\nDigite a terceira nota do aluno: ')))
+
+print(notas)
