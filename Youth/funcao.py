d389c6a6a73bfbe354bf19ea874417bf82280f98
Douglas Nunes <paulodnunes2016@gmail.com>
2025-03-16 11:24:47 -0300

    Tabuada de 1 a 10

--- Youth/funcao.py
+++ Youth/funcao.py
@@ -4,7 +4,35 @@
 
 # tabuada(10)
 for i in range(1,11):
+    print(f'1 x {i} = {1 * i}')
+print()
+for i in range(1,11):
+    print(f'2 x {i} = {2 * i}')
+print()
+for i in range(1,11):
+    print(f'3 x {i} = {3 * i}')
+print()
+for i in range(1,11):
+    print(f'4 x {i} = {4 * i}')
+print()
+for i in range(1,11):
+    print(f'5 x {i} = {5 * i}')
+print()
+for i in range(1,11):
+    print(f'6 x {i} = {6 * i}')
+print()
+for i in range(1,11):
+    print(f'7 x {i} = {7 * i}')
+print()
+for i in range(1,11):
+    print(f'8 x {i} = {8 * i}')
+print()
+for i in range(1,11):
+    print(f'9 x {i} = {9 * i}')
+print()
+for i in range(1,11):
     print(f'10 x {i} = {10 * i}')
+print()
 
   
    
