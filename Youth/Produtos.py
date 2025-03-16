8d2513f5423ae2e66ba478f89302311a30a417e5
Douglas Nunes <paulodnunes2016@gmail.com>
2025-03-16 12:24:26 -0300

    Produtos

---
+++ Youth/Produtos.py
@@ -0,0 +1,25 @@
+class Produto:
+    def __init__(self,nome,preco):
+        self.nome = nome
+        self.preco = preco
+    def get_preco(self):
+        return self.get_preco
+    def set_preco(self,novoPreco):
+        if novoPreco > 0:
+          self.get_preco = novoPreco
+        else:
+            print('Preço inválido')
+    
+Produto1 = Produto('Telemovél',1500)
+exibir = input('\nDigite o valor do produto\n')
+print(Produto1.preco)
+Produto1.preco = 2000
+print(Produto1.preco)
+Produto1.preco = - 100
+print(Produto1.preco)
+
+
+
+    
+        
+        
