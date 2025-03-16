18f0965c725f6d067d8dd2040806f8f8d70e0608
Douglas Nunes <paulodnunes2016@gmail.com>
2025-03-16 11:10:14 -0300

    Conta bancaria

--- FaculdadeCdl/Listas.py
+++ FaculdadeCdl/Listas.py
@@ -6,9 +6,26 @@
 # for i in range(3):
 #     print(notas[i])
 
-notas = []
-notas.append(float(input('\nDigite a primeira nota do aluno: ')))
-notas.append(float(input('\nDigite a segunda nota do aluno: ')))
-notas.append(float(input('\nDigite a terceira nota do aluno: ')))
+# notas = []
+# notas.append(float(input('\nDigite a primeira nota do aluno: ')))
+# notas.append(float(input('\nDigite a segunda nota do aluno: ')))
+# notas.append(float(input('\nDigite a terceira nota do aluno: ')))
+# print(notas)
 
-print(notas)
+
+numeroAlunos = 3
+media = 0
+nome = []
+notas = []
+
+for i in range(numeroAlunos):
+    nome.append(input('Digite o nome do aluno: '))
+    notas.append(float(input(f'Informe a nota de ' +nome[i] + ': ')))
+    media = media + notas[i]
+
+media = media / notas
+print('A média dos alunos é de ',media)
+
+for i in range(numeroAlunos):
+    if notas[i] > media:
+        print('Parabéns',nome[i])
---
+++ Youth/Criarproduto.py
@@ -0,0 +1,40 @@
+class Contabancaria():
+    def __init__(self,nome,saldo = 0):
+        self.nome = nome
+        self.saldo = saldo
+    def depositar(self,valor):
+        self.saldo += valor
+        print(f'O valor depositado com sucesso,seu salário novo é de:{self.saldo}')
+    def sacar(self,valor):
+        if valor <= self.saldo:
+            self.saldo -= valor
+        else:
+            print('Voce não possui saldo suficiente.')
+    def mostrarSaldo(self):
+        print(f'Seu saldo é de: {self.saldo}')
+
+conta = Contabancaria(1200,'João')
+while True:
+    opcao = input('1- Depositar\n2- Sacar\n3- Mostar Saldo\n4- Sair\n')
+    if opcao == 'Sair':
+        print('Saindo...')
+        break
+    elif opcao == '1':
+        valor = float(input('Digite o valor depositado!'))
+        conta.depositar(valor)
+    elif opcao == '2':
+        valor = float(input('Digite o valor a ser sacado:'))
+        conta.sacar(valor)
+    elif opcao == '3':
+        conta.mostrarSaldo()
+        
+    
+
+
+    
+       
+       
+
+
+
+    
